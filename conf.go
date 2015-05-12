080578de738bf1b666878522e9ac922d9affe90c
brg-liuwei <stupidlw@126.com>
2015-05-12 19:26:06 +0800

    f.close

--- conf.go
+++ conf.go
@@ -7,19 +7,25 @@ import (
 )
 
 func DecodeJsonFile(filename string, schema interface{}) error {
-	if f, err := os.Open(filename); err != nil {
+	f, err := os.Open(filename)
+	if err != nil {
 		return err
-	} else {
-		decoder := json.NewDecoder(f)
-		return decoder.Decode(schema)
 	}
+	defer f.Close()
+
+	decoder := json.NewDecoder(f)
+	return decoder.Decode(schema)
 }
 
 func ParseJsonFileToMap(filename string) (map[string]interface{}, error) {
 	var m map[string]interface{}
-	if f, err := os.Open(filename); err != nil {
+	f, err := os.Open(filename)
+	if err != nil {
 		return nil, err
-	} else if b, err := ioutil.ReadAll(f); err != nil {
+	}
+	defer f.Close()
+
+	if b, err := ioutil.ReadAll(f); err != nil {
 		return nil, err
 	} else if err = json.Unmarshal(b, &m); err != nil {
 		return nil, err
@@ -30,9 +36,13 @@ func ParseJsonFileToMap(filename string) (map[string]interface{}, error) {
 
 func ParseJsonFileToSlice(filename string) ([]interface{}, error) {
 	var s []interface{}
-	if f, err := os.Open(filename); err != nil {
+	f, err := os.Open(filename)
+	if err != nil {
 		return nil, err
-	} else if b, err := ioutil.ReadAll(f); err != nil {
+	}
+	defer f.Close()
+
+	if b, err := ioutil.ReadAll(f); err != nil {
 		return nil, err
 	} else if err = json.Unmarshal(b, &s); err != nil {
 		return nil, err
