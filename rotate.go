58c8381139b9861616be2749a019b4fcff5666d3
董家宏 <dongjiahong@hotmail.com>
2017-01-13 13:44:44 +0800

    delete annotation

--- rotate.go
+++ rotate.go
@@ -83,7 +83,6 @@ func (rlogger *RotateLogger) forceRotateWithLock() {
 		}
 	}
 	rlogger.fp, _ = os.OpenFile(rlogger.absPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
-	//rlogger.logger = log.New(rlogger.fp, rlogger.prefix, rlogger.flag)
 	rlogger.logger.SetOutput(rlogger.fp)
 }
 
